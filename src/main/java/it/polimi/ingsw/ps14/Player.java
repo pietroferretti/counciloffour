5ebd5c33b7f05080acbb70ce6aa9107c531732a3
Pietro Ferretti <pietro.ferretti1@gmail.com>
2016-05-06 23:37:44 +0200

    GameLogic is not empty anymore

--- src/main/java/it/polimi/ingsw/ps14/Player.java
+++ src/main/java/it/polimi/ingsw/ps14/Player.java
@@ -5,40 +5,64 @@ import java.util.Scanner;
 
 public class Player {
 
-	private String color;
+	private final String name;
+	private final String color; // TODO: magari cambiare da stringa a qualche
+								// tipo come java.awt.Color boh
 	private int coins;
-	private PoliticDeck hand;
-	private int level;
+	private int assistants;
+	private int level; // nobility
 	private int points;
+	private ArrayList<PoliticCard> hand;
 	private ArrayList<BusinessPermit> permitTiles;
 	private ArrayList<BusinessPermit> usedPermitTiles;
-	private int assistants;
 
-	public Player(PoliticDeck hand, ArrayList<BusinessPermit> permitTiles, int coins, int assistants) {
-		chooseColor();
+	public Player(String name, String color, int coins, int assistants, PoliticDeck deck, int numberOfCards) {
+		this.name = name;
+		this.color = color;
 		this.coins = coins;
 		this.assistants = assistants;
 		level = 0;
 		points = 0;
-		this.hand = hand;
-		this.permitTiles = permitTiles;
-		usedPermitTiles = null;
+		this.hand = deck.drawMultipleCards(numberOfCards);
+		permitTiles = new ArrayList<BusinessPermit>();
+		usedPermitTiles = new ArrayList<BusinessPermit>();
+	}
+
+	public Player(String name, String color) {
+		this.name = name;
+		this.color = color;
+		coins = 0;
+		assistants = 0;
+		level = 0;
+		points = 0;
+		hand = new ArrayList<PoliticCard>();
+		permitTiles = new ArrayList<BusinessPermit>();
+		usedPermitTiles = new ArrayList<BusinessPermit>();
 	}
 
 	public void chooseColor() {
-		// TODO Should we read it from the config file instead?
-		// or maybe we should ask for a player NAME .-.
+
+		// se dobbiamo chiederlo al giocatore mi sa che è meglio chiederlo
+		// insieme al nome quando il giocatore si collega
+		// non in questa classe
 
 		// terminale
-		Scanner input = new Scanner(System.in);
-		System.out.println("Choose your color: "); // dovremmo mettere dei
-													// controlli?? se mettiamo
-													// nome invece di colore
-													// viene più facile
-		color = input.next();
-		input.close();
+		/*
+		 * Scanner input = new Scanner(System.in); System.out.println(
+		 * "Choose your color: "); // dovremmo mettere dei // controlli?? se
+		 * mettiamo // nome invece di colore // viene più facile color =
+		 * input.next(); input.close();
+		 */
 	}
 
+	public String getName() {
+		return name;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
 	public boolean useCoins(int coins) {
 		if (this.coins >= coins) {
 			this.coins = this.coins - coins;
@@ -51,7 +75,11 @@ public class Player {
 		return coins;
 	}
 
-	public boolean useAssistant(int assistantNumber) {
+	public void addCoins(int coins) {
+		this.coins = this.coins + coins;
+	}
+
+	public boolean useAssistants(int assistantNumber) {
 		if (assistants >= assistantNumber) {
 			assistants = assistants - assistantNumber;
 			return true;
@@ -59,37 +87,60 @@ public class Player {
 			return false;
 	}
 
-	public int getAssistant() {
+	public int getAssistants() {
 		return assistants;
 	}
 
-	public String getColor() {
-		return color;
+	public void addAssistants(int assistants) {
+		this.assistants = this.assistants + assistants;
 	}
 
 	public int getLevel() {
 		return level;
 	}
 
-	public void upLevel() {
+	public int upLevel() {
 		level++;
+		return level;	// returns the new value of level to check bonuses
 	}
-
-	public void acquireBusinessPermit(BusinessDeck deck) {
-		permitTiles.add(deck.drawCard());
-		// TODO possibili errori?
-		// tipo mazzo che finisce ecc li controlliamo prima di invocarlo?
-	}
-
-	public PoliticDeck getHand() {
-		return hand;
-	}
-
+	
 	public int getPoints() {
 		return points;
 	}
 
-	public int getAssistants() {
-		return assistants;
+	public void addPoints(int points) {
+		this.points = this.points + points;
 	}
+	
+	public ArrayList<PoliticCard> getHand() {
+		return hand;
+	}
+	
+	public int getNumberOfCards() {
+		return hand.size();
+	}
+	
+	public void drawCard(PoliticDeck deck) {
+		hand.add(deck.drawCard());
+		// TODO possibili errori?
+		// tipo mazzo che finisce ecc li controlliamo prima di invocarlo?
+	}
+	
+	// sì può accorpare all'altro metodo con polimorfismo?
+	public void drawMultipleCards(PoliticDeck deck, int numberOfCards) {
+		hand.addAll(deck.drawMultipleCards(numberOfCards));
+	}
+
+	public void useCards(){
+		//???
+	}
+	
+	public void acquireBusinessPermit(BusinessPermit permitTile) {
+		permitTiles.add(permitTile);
+	}
+	
+	public int getNumberOfPermits() {
+		return permitTiles.size() + usedPermitTiles.size();
+	}
+
 }
--- src/main/java/it/polimi/ingsw/ps14/PoliticDeck.java
+++ src/main/java/it/polimi/ingsw/ps14/PoliticDeck.java
@@ -5,17 +5,33 @@ import java.util.ArrayList;
 public class PoliticDeck implements Deck {
 
 	public ArrayList<PoliticCard> deck;
-	
+
+	/*
+	 * TODO: riscrivere il costruttore in modo che prenda i parametri salvati su
+	 * file e costruisca carta per carta?
+	 */
+	public PoliticDeck(ArrayList<PoliticCard> deck) {
+		this.deck = deck;
+	}
+
 	public void shuffle() {
-	
+		//TODO
 	}
 
 	public void shuffleAll() {
-		
+		//TODO
 	}
-	
+
 	public PoliticCard drawCard() {
 		return new PoliticCard();
+	}
+
+	public ArrayList<PoliticCard> drawMultipleCards(int number) {
+		ArrayList<PoliticCard> result = new ArrayList<PoliticCard>();
+		for (int i = 0; i < number; i++) {
+		    result.add(drawCard());
+		}
+		return result;
 	}
 
 }
