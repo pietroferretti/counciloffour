ca5283a2346d64a5951b53209c3c5c78f98e7355
Pietro Ferretti <pietro.ferretti1@gmail.com>
2016-05-10 22:16:26 +0200

    First draft of the JSON parser (in Settings)

--- src/main/java/it/polimi/ingsw/ps14/Settings.java
+++ src/main/java/it/polimi/ingsw/ps14/Settings.java
@@ -1,21 +1,49 @@
 package it.polimi.ingsw.ps14;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class Settings {
 
-  private ArrayList<City> cities;
-  
-  int availableCouncillorsEachColor;
+	final Map completeSettings;
+	
+	final ArrayList<City> cities;
 
-  int assistantAvailable;
-  
-  City startCityKing;
-  
-  public void readSettings() {
-  }
+	final int availableCouncillorsEachColor;
 
-  public Settings() {
-  }
+	final int availableAssistants;
+
+	final City startCityKing;
+
+	public Settings(String filename) throws IOException {
+		BufferedReader settingsFile;
+		try {
+			settingsFile = new BufferedReader(new FileReader(filename));
+		} catch (FileNotFoundException e) {
+			throw new FileNotFoundException("Couldn't open settings file on path '" + filename + "'");
+		}
+
+		JSONTokener jsonFile = new JSONTokener(settingsFile);
+		JSONObject jsonMap = (JSONObject) jsonFile.nextValue();
+		availableAssistants = jsonMap.getInt("assistants");
+		availableCouncillorsEachColor = jsonMap.getInt("councillors");
+		
+		cities = null;
+		startCityKing = null;
+		completeSettings = null;
+	
+		try {
+			settingsFile.close();
+		} catch (IOException e) {
+			throw new IOException("Couldn't close settings file");
+		}
+	}
 
 }
