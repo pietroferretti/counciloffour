9cf5799d6602105769d78e491ceddd98f9f8234e
Nicole Gervasoni <nicole.gervasoni@gmail.com>
2016-05-24 13:43:25 +0200

    Written ChooseMainWhenAlreadyDoneTurnState

--- src/main/java/it/polimi/ingsw/ps14/controller/turnstates/ChooseMainWhenAlreadyDoneTurnState.java
+++ src/main/java/it/polimi/ingsw/ps14/controller/turnstates/ChooseMainWhenAlreadyDoneTurnState.java
@@ -1,13 +1,27 @@
 package it.polimi.ingsw.ps14.controller.turnstates;
 
 import it.polimi.ingsw.ps14.controller.actions.Action;
+import it.polimi.ingsw.ps14.controller.actions.mainactions.AcquireBusinessPermiteTileAction;
+import it.polimi.ingsw.ps14.controller.actions.mainactions.BuildEmporiumUsingPermitTileAction;
+import it.polimi.ingsw.ps14.controller.actions.mainactions.BuildEmporiumWithHelpOfKingAction;
+import it.polimi.ingsw.ps14.controller.actions.mainactions.ElectCouncillorAction;
 
 public class ChooseMainWhenAlreadyDoneTurnState extends TurnState {
 
+	private static final ChooseMainWhenAlreadyDoneTurnState INSTANCE = new ChooseMainWhenAlreadyDoneTurnState();
+
+	public static ChooseMainWhenAlreadyDoneTurnState getInstance() {
+		return INSTANCE;
+	}
+
 	@Override
 	public boolean isActionValid(Action action) {
-		// TODO Auto-generated method stub
-		return false;
+
+		return action.isValid()
+				&& (action instanceof ElectCouncillorAction || action instanceof AcquireBusinessPermiteTileAction
+						|| action instanceof BuildEmporiumUsingPermitTileAction
+						|| action instanceof BuildEmporiumWithHelpOfKingAction);
+
 	}
 
 }
