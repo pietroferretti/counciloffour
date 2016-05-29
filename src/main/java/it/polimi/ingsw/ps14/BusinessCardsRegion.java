b32ede1321ce8a4616ca43ed1c5c1eb8a7f0ea82
Nicole Gervasoni <nicole.gervasoni@gmail.com>
2016-05-29 18:38:12 +0200

    Added getter for available region permits

--- src/main/java/it/polimi/ingsw/ps14/BusinessCardsRegion.java
+++ src/main/java/it/polimi/ingsw/ps14/BusinessCardsRegion.java
@@ -8,38 +8,42 @@ public class BusinessCardsRegion {
 
 	private List<BusinessPermit> deck;
 	private BusinessPermit[] availablePermit;
-	
-	public BusinessCardsRegion(ArrayList<BusinessPermit> deck){
-		this.deck=deck;
-		availablePermit= new BusinessPermit[2];
+
+	public BusinessCardsRegion(ArrayList<BusinessPermit> deck) {
+		this.deck = deck;
+		availablePermit = new BusinessPermit[2];
 	}
-	
-	public BusinessCardsRegion(){
-		availablePermit= new BusinessPermit[2];
+
+	public BusinessCardsRegion() {
+		availablePermit = new BusinessPermit[2];
 	}
-	
-	public void shuffle(){
+
+	public void shuffle() {
 		Collections.shuffle(deck);
 	}
-	
+
 	private BusinessPermit drawCardFromDeck() {
 		BusinessPermit card = (BusinessPermit) deck.remove(0);
 		return card;
 	}
-	
 
 	public void substituteCard(BusinessPermit toSubstitute) {
-		for(BusinessPermit busPer: availablePermit)
-			if(busPer==toSubstitute){
-				busPer=drawCardFromDeck();
+		for (BusinessPermit busPer : availablePermit)
+			if (busPer == toSubstitute) {
+				busPer = drawCardFromDeck();
 			}
 	}
-	public boolean cardIsChoosable(BusinessPermit card){
-		for(BusinessPermit busPer: availablePermit)
-			if(busPer.equals(card)){
+
+	public boolean cardIsChoosable(BusinessPermit card) {
+		for (BusinessPermit busPer : availablePermit)
+			if (busPer.equals(card)) {
 				return true;
 			}
 		return false;
 	}
-	
+
+	public BusinessPermit[] getAvailablePermit() {
+		return availablePermit;
+	}
+
 }
