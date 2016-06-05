e1b3653d6360c7c4bf537db8fa0d62d67b902037
Federico <Federico@Fede.station>
2016-06-05 17:51:40 +0200

    AcquireBusinessPermiteTileAction TEST

---
+++ src/main/java/it/polimi/ingsw/ps14/controller/actions/mainactions/AcquireBusinessPermiteTileActionTest.java
@@ -0,0 +1,47 @@
+package it.polimi.ingsw.ps14.controller.actions.mainactions;
+
+import static org.junit.Assert.assertEquals;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import it.polimi.ingsw.ps14.ColorPolitic;
+import it.polimi.ingsw.ps14.GameBoard;
+import it.polimi.ingsw.ps14.Player;
+import it.polimi.ingsw.ps14.PoliticCard;
+import it.polimi.ingsw.ps14.RegionType;
+import it.polimi.ingsw.ps14.Settings;
+
+public class AcquireBusinessPermiteTileActionTest {
+
+	@Test
+	public void test() {
+		Settings settingsInstance = null;
+		try {
+			settingsInstance = new Settings("settings.json");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		GameBoard gameboard = new GameBoard(settingsInstance);
+		
+		List<PoliticCard> cards=new ArrayList<>();
+//		cards.add(new PoliticCard(ColorPolitic.BLACK));
+		cards.add(new PoliticCard(ColorPolitic.JOLLY));
+//		cards.add(new PoliticCard(ColorPolitic.PINK));
+		Player player=new Player("ubaldo",Color.DARK_GRAY,20,12,gameboard.getPoliticDeck(),6);
+
+		System.out.println(player.toString());
+		
+		MainAction action = new AcquireBusinessPermiteTileAction(player, gameboard, gameboard.getRegion(RegionType.COAST), gameboard.getRegion(RegionType.COAST).getBusinessPermits().getAvailablePermits()[0], cards);
+		assertEquals(action.isValid(),true);
+		
+		System.out.println(player.toString());
+
+	}
+
+}
