0028e031b8d2736c7d122a8aa74b9531aa8dd0f5
Nicole Gervasoni <nicole.gervasoni@gmail.com>
2016-06-07 22:33:52 +0200

    Added Game constructor

--- src/main/java/it/polimi/ingsw/ps14/model/Game.java
+++ src/main/java/it/polimi/ingsw/ps14/model/Game.java
@@ -1,17 +1,29 @@
 package it.polimi.ingsw.ps14.model;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 
 public class Game extends Observable implements Cloneable {
-	
+
 	private GameBoard gameBoard;
 	private List<Player> players;
-	
+
+	public Game() {
+		try {
+			gameBoard = new GameBoard(new Settings("settings.json"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		players = new ArrayList<>();
+	}
+
 	public void setGameBoard(GameBoard gameBoard) {
 		this.gameBoard = gameBoard;
 	}
-	
+
 	public void setPlayers(List<Player> players) {
 		this.players = players;
 	}
@@ -33,16 +45,16 @@ public class Game extends Observable implements Cloneable {
 	// colore univoco che usiamo come chiave
 	public Player getIDPlayer(int id) {
 		for (Player player : players) {
-			if (player.getId()==id)
+			if (player.getId() == id)
 				return player;
 		}
 		// magari una exception?
 		return null;
 	}
-	
+
 	@Override
-	public Game clone() throws CloneNotSupportedException{
-		// TODO 
+	public Game clone() throws CloneNotSupportedException {
+		// TODO
 		throw new CloneNotSupportedException();
 	}
 
