7946207420b1d8f4866a124db493042f8564c33d
Nicole Gervasoni <nicole.gervasoni@gmail.com>
2016-06-16 17:02:21 +0200

    Big Serialization ;)

--- src/main/java/it/polimi/ingsw/ps14/model/modelview/PlayerView.java
+++ src/main/java/it/polimi/ingsw/ps14/model/modelview/PlayerView.java
@@ -1,12 +1,17 @@
 package it.polimi.ingsw.ps14.model.modelview;
 
+import java.io.Serializable;
 import java.util.Observable;
 import java.util.Observer;
 
 import it.polimi.ingsw.ps14.model.Player;
 
-public class PlayerView extends Observable implements Observer {
+public class PlayerView extends Observable implements Observer, Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7718233332405533853L;
 	private Player playerCopy;
 
 	public PlayerView(Player playerCopy) {
