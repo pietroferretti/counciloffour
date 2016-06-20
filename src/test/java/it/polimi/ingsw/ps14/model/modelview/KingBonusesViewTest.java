40328175b57f8165f160c59244e2950c2ea05d84
Nicole Gervasoni <nicole.gervasoni@gmail.com>
2016-06-20 18:04:48 +0200

    KingBonusesView Test

---
+++ src/test/java/it/polimi/ingsw/ps14/model/modelview/KingBonusesViewTest.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.ps14.model.modelview;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import it.polimi.ingsw.ps14.model.Model;
+import it.polimi.ingsw.ps14.model.Player;
+
+public class KingBonusesViewTest {
+	private Model model;
+	private Player player;
+	private KingBonusesView kbv;
+
+	@Before
+	public void setUp() throws Exception {
+		model = new Model();
+		player = new Player();
+		kbv = new KingBonusesView(model.getGameBoard().getKingBonuses().peek());
+		model.getGameBoard().addObserver(kbv);
+	}
+
+	@Test
+	public void testKingBonusesView() {
+		KingBonusesView kbv1 = new KingBonusesView(model.getGameBoard().getKingBonuses().peek());
+		assertEquals(model.getGameBoard().getKingBonuses().peek().intValue(), kbv1.getShowableKingBonus());
+	}
+
+	@Test
+	public void testUpdate() {
+		System.out.println("\n---------testUpdate---------\n");
+		System.out.println(model.getGameBoard().getKingBonuses().toString());
+		System.out.println(Integer.toString(kbv.getShowableKingBonus()));
+
+		System.out.println("Use bonus");
+		model.getGameBoard().useKingBonus();
+
+		assertEquals(model.getGameBoard().getKingBonuses().peek().intValue(), kbv.getShowableKingBonus());
+
+		System.out.println(model.getGameBoard().getKingBonuses().toString());
+		System.out.println(Integer.toString(kbv.getShowableKingBonus()));
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testUpdateException() {
+		player.addObserver(kbv);
+		player.addCoins(4);
+	}
+
+}
