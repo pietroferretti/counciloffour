52c4749ef5f932d03a5d3889eed185d148fc8b08
federico <federico.oldani@gmail.com>
2016-06-20 16:54:04 +0200

    created trash

--- src/main/java/it/polimi/ingsw/ps14/client/Client.java
+++ src/main/java/it/polimi/ingsw/ps14/client/Client.java
@@ -1,17 +1,20 @@
 package it.polimi.ingsw.ps14.client;
 
-import it.polimi.ingsw.ps14.client.RMI.ClientRMI;
-import it.polimi.ingsw.ps14.client.socket.ClientSocket;
+import it.polimi.ingsw.ps14.client.RMI.ClientRMIView;
 import it.polimi.ingsw.ps14.client.socket.SocketMessageHandlerIn;
 import it.polimi.ingsw.ps14.client.socket.SocketMessageHandlerOut;
 import it.polimi.ingsw.ps14.message.fromclient.PlayerNameMsg;
+import it.polimi.ingsw.ps14.server.RMIViewRemote;
 import it.polimi.ingsw.ps14.view.CLIView;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,17 +22,26 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Client {
-	private static final Logger LOGGER = Logger.getLogger(Client.class.getName());
+
+	private final static int RMI_PORT = 52365;
+	private static final String NAME = "council4";
+	private final static String HOST = "127.0.0.1";
+	private static final int PORT = 19999;
+
+
+	private static final Logger LOGGER = Logger.getLogger(Client.class
+			.getName());
 
 	private Client() {
 	};
 
-	public static void main(String[] args) throws IOException, NotBoundException, AlreadyBoundException {
+	public static void main(String[] args) throws IOException,
+			NotBoundException, AlreadyBoundException {
 
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Welcome to Council of Four!");
-		
+
 		System.out.println("Choose a name:");
 		String name = scanner.nextLine();
 
@@ -42,43 +54,63 @@ public class Client {
 			input = scanner.nextLine();
 		} while (!input.toUpperCase().matches("^(SOCKET|RMI)$"));
 
-
 		if (input.toUpperCase().matches("^(SOCKET)$")) {
-
+			Socket socket;
 			try {
-			
-				ClientSocket socket = new ClientSocket();
-			
-				SocketMessageHandlerOut msgHandlerOut = new SocketMessageHandlerOut(new ObjectOutputStream(socket.getOutputStream()));
-				
+
+				try {
+
+					socket = new Socket(HOST, PORT);
+					System.out.println("Connection created");
+
+				} catch (IOException e) {
+
+					LOGGER.warning(String.format(
+							"Couldn't create socket at IP '%s' on port '%d'",
+							HOST, PORT));
+					throw e;
+
+				}
+
+				SocketMessageHandlerOut msgHandlerOut = new SocketMessageHandlerOut(
+						new ObjectOutputStream(socket.getOutputStream()));
+
 				clientView.setHandlerOut(msgHandlerOut);
-				
-				MessageHandlerIn msgHandlerIn = new SocketMessageHandlerIn(clientView, new ObjectInputStream(socket.getInputStream()));
-	
+
+				MessageHandlerIn msgHandlerIn = new SocketMessageHandlerIn(
+						clientView, new ObjectInputStream(
+								socket.getInputStream()));
+
 				// sends the player name to the server
 				clientView.handleMessageOut(new PlayerNameMsg(name));
-				
+
 				ExecutorService executor = Executors.newFixedThreadPool(2);
 				executor.submit(clientView);
 				executor.submit(msgHandlerIn);
-			
+
 			} catch (IOException e) {
-				
-				LOGGER.log(Level.WARNING, "Couldn't create connection, closing.", e);
-				
+
+				LOGGER.log(Level.WARNING,
+						"Couldn't create connection, closing.", e);
+
 			}
-			
+
 		} else if (input.toUpperCase().matches("^(RMI)$")) {
-			LOGGER.warning("RMI not yet implemented");
-			
-			ClientRMI rmi= new ClientRMI();
-			
+
+			Registry registry = LocateRegistry.getRegistry(HOST, RMI_PORT);
+			RMIViewRemote serverStub = (RMIViewRemote) registry.lookup(NAME);
+
+			ClientRMIView rmiView = new ClientRMIView();
+
+			serverStub.registerClient(rmiView);
+
 			CLIView cli = new CLIView(scanner);
 			cli.run();
-			
+
 		} else {
 			scanner.close();
-			throw new IllegalArgumentException("Something went wrong while parsing the connection type");
+			throw new IllegalArgumentException(
+					"Something went wrong while parsing the connection type");
 		}
 
 		// ?
