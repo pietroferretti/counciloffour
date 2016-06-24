dd0364dbad122fe781e544ab41b0157c108f9ad9
Pietro Ferretti <pietro.ferretti1@gmail.com>
2016-06-24 14:18:41 +0200

    First set of tests for Controller

--- src/main/java/it/polimi/ingsw/ps14/model/turnstates/TurnState.java
+++ src/main/java/it/polimi/ingsw/ps14/model/turnstates/TurnState.java
@@ -6,31 +6,35 @@ import it.polimi.ingsw.ps14.exceptions.IllegalActionException;
 import it.polimi.ingsw.ps14.model.Model;
 import it.polimi.ingsw.ps14.model.actions.Action;
 
-public abstract class TurnState implements Serializable{
-	
+public abstract class TurnState implements Serializable {
 
 	private static final long serialVersionUID = -8394194912933055049L;
-	
+
 	public final int additionalMains;
-	
+
 	public TurnState(int additionalMains) {
 		this.additionalMains = additionalMains;
 	}
-	
+
 	/**
 	 * Prototype Pattern!
 	 */
 	public abstract TurnState makeCopy();
-	
-	// check if we can do an action based on the TurnState we are in
+
+	/**
+	 * Checks if we can do an action based on the TurnState we are in
+	 * 
+	 * @param action
+	 * @param model
+	 * @return
+	 */
 	public abstract boolean isActionLegal(Action action, Model model);
 
 	public TurnState executeAction(Action action, Model model) {
-		
+
 		if (!isActionLegal(action, model)) {
-			System.out.println("NON PUOI FARE QUESTA AZIONE");//TODO remove
 			throw new IllegalActionException("You cannot do this action now!");
-		} 
+		}
 
 		return action.execute(this, model);
 
