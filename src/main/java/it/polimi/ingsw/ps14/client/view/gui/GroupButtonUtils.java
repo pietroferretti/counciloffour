55794db3eda613e81a63608b37fe8322e5acba5c
Nicole Gervasoni <nicole.gervasoni@gmail.com>
2016-06-28 21:22:57 +0200

    gui :)

---
+++ src/main/java/it/polimi/ingsw/ps14/client/view/gui/GroupButtonUtils.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.ps14.client.view.gui;
+
+import java.util.Enumeration;
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+
+public class GroupButtonUtils
+{
+
+    public String getSelectedButtonText(ButtonGroup buttonGroup) {
+        for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
+            AbstractButton button = buttons.nextElement();
+
+            if (button.isSelected()) {
+                return button.getText();
+            }
+        }
+
+        return null;
+    }
+}
