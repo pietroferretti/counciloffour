90d6222454027382a663559a9c1ad17eba368d6e
Nicole Gervasoni <nicole.gervasoni@gmail.com>
2016-07-05 18:23:37 +0200

    remove unused classes

--- src/main/java/it/polimi/ingsw/ps14/classiDaEliminare/MessageHandlerOut.java
+++
@@ -1,9 +0,0 @@
-//package it.polimi.ingsw.ps14.classiDaEliminare;
-//package it.polimi.ingsw.ps14.client;
-//
-//import it.polimi.ingsw.ps14.message.Message;
-//
-//public interface MessageHandlerOut {
-//
-//	public void sendMessage(Message message);
-//}
