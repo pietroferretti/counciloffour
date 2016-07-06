f2a9c09ce7c15ed634c865d7bd8e22b2688a53b0
Nicole Gervasoni <nicole.gervasoni@gmail.com>
2016-07-06 19:38:32 +0200

    Javadoc

--- src/main/java/it/polimi/ingsw/ps14/client/view/ClientView.java
+++ src/main/java/it/polimi/ingsw/ps14/client/view/ClientView.java
@@ -5,23 +5,24 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import it.polimi.ingsw.ps14.client.Client;
 import it.polimi.ingsw.ps14.client.Communication;
+import it.polimi.ingsw.ps14.controller.Controller;
 import it.polimi.ingsw.ps14.message.Message;
 import it.polimi.ingsw.ps14.model.ColorCouncillor;
 import it.polimi.ingsw.ps14.model.ItemForSale;
 import it.polimi.ingsw.ps14.model.King;
 import it.polimi.ingsw.ps14.model.Market;
+import it.polimi.ingsw.ps14.model.Model;
 import it.polimi.ingsw.ps14.model.NobilityTrack;
 import it.polimi.ingsw.ps14.model.Player;
 import it.polimi.ingsw.ps14.model.Region;
 import it.polimi.ingsw.ps14.model.State;
 
 /**
- * this public abstract class is unique for cli and gui, the back end call these
- * methods and this class use method of cliView or GuiView according to type
- * chosen.
+ * It's the view on the {@link Client} side; it receives {@link Model}'s updates
+ * and send infos to the {@link Controller} using a communication type.
  * 
- * @author federico
  *
  */
 public abstract class ClientView implements Runnable {
@@ -62,7 +63,7 @@ public abstract class ClientView implements Runnable {
 	public void setGameStarted(boolean gameStarted) {
 		this.gameStarted = gameStarted;
 	}
-	
+
 	public abstract void loadMap(String mapName) throws IOException;
 
 	public abstract void readMessage(Message message);
@@ -73,13 +74,12 @@ public abstract class ClientView implements Runnable {
 
 	public abstract void showAvailableCommands();
 
-	public abstract void showAvailableCouncillor(
-			Map<ColorCouncillor, Integer> updatedAvailableCouncillors);
+	public abstract void showAvailableCouncillor(Map<ColorCouncillor, Integer> updatedAvailableCouncillors);
 
 	public abstract void showChatMsg(String author, String text);
 
-	public abstract void showCitiesColorBonuses(int updatedBonusGold,
-			int updatedBonusSilver, int updatedBonusBronze, int updatedBonusBlue);
+	public abstract void showCitiesColorBonuses(int updatedBonusGold, int updatedBonusSilver, int updatedBonusBronze,
+			int updatedBonusBlue);
 
 	public abstract void showEndGame(List<List<String>> endResults);
 
@@ -98,12 +98,12 @@ public abstract class ClientView implements Runnable {
 	public abstract void showNobilityTrack(NobilityTrack updatedNobilityTrack);
 
 	// doesn't take Player as a parameter to keep other players' info safe
-	public abstract void showOtherPlayer(int id, String name, Color color,
-			int coins, int assistants, int level, int points, int numEmporiums);
+	public abstract void showOtherPlayer(int id, String name, Color color, int coins, int assistants, int level,
+			int points, int numEmporiums);
 
 	public abstract void showPersonalDetails(Player p);
 
-	public abstract void showPlayerChangesPrivate(Player player,String message);
+	public abstract void showPlayerChangesPrivate(Player player, String message);
 
 	public abstract void showPlayerChangesPublic(String notice);
 
